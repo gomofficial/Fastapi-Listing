11afc9a4361d18e0e9f363476e98a67a925e827d
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-05-18 14:16:06 +0330

    exception handling for user operations

---
+++ exceptions.py
@@ -0,0 +1,18 @@
+from typing import Any, Dict
+from typing_extensions import Annotated, Doc
+from fastapi import HTTPException, status
+
+class UserNotFoundError(HTTPException):
+    def __init__(self):
+        self.status_code = status.HTTP_404_NOT_FOUND
+        self.detail="User not found"
+
+class UserAlreadyExists(HTTPException):
+    def __init__(self):
+        self.status_code = status.HTTP_400_BAD_REQUEST
+        self.detail="User already exists"
+
+class UserAuthenticationError(HTTPException):
+    def __init__(self):
+        self.status_code = status.HTTP_400_BAD_REQUEST
+        self.detail="wrong username or password"
