81b2863127c66bdc12456df7a2f6642117f3c6e9
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-05-18 11:17:13 +0330

    fixed

--- settings.py
+++ settings.py
@@ -1,9 +1,4 @@
-from os import environ as env
-
-
 SECRET_KEY:str = 'SOME_SECRET_KEY'
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./todo.db"
-
-
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./fastapi_listing.db"
 ACCESS_TOKEN_EXPIRE_MINUTES:int = 30
 ALGORITHM:str = "HS256"
