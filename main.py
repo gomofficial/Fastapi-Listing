e152bd5b179d402e2683bb98b03d5bcb6db506ed
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-05-18 11:16:47 +0330

    router name changed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers.users import auth_router
+from routers import user_router
 from db import engine, Base
 
 app = FastAPI()
@@ -10,4 +10,4 @@ async def init_table():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-app.include_router(auth_router, prefix='/account', tags=['account'])
+app.include_router(user_router, prefix='/account', tags=['account'])
