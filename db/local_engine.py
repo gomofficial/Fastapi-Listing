e8eed7885d16b54a231f5408075e5134d24a0db1
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-05-21 15:23:08 +0330

    local engine for seeder data

---
+++ db/local_engine.py
@@ -0,0 +1,29 @@
+import os, sys, inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from utils.secrets import password_manager
+from utils.enums import GenderEnum
+from settings import *
+
+SQLALCHEMY_DATABASE_URL = SQLALCHEMY_LOCAL_DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(
+                            bind=engine,
+                            autocommit=False,
+                            autoflush=False,
+                            expire_on_commit=False,
+                            )
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
