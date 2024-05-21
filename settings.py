97f2426a1aefae2cadd8437b35a02aa73cb8e59b
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-05-21 15:24:06 +0330

    updating sql url

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 SECRET_KEY:str = 'SOME_SECRET_KEY'
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./fastapi_listing.db"
+# SQLALCHEMY_SQLITE_DATABASE_URL = "sqlite:///./fastapi_listing.db"
 SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:password@db:5432/post_db"
+SQLALCHEMY_LOCAL_DATABASE_URL = "postgresql+psycopg2://postgres:password@db:5432/post_db"
 ACCESS_TOKEN_EXPIRE_MINUTES:int = 30
 ALGORITHM:str = "HS256"
 
