cd1c3ca6611bb54fd62561ad74d55eab60da6278
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-06-07 23:35:37 +0330

    outputs should be nullable

--- schema/output.py
+++ schema/output.py
@@ -1,14 +1,16 @@
 from pydantic import BaseModel
+from typing import Union
 from uuid import UUID
 from datetime import datetime
 from utils.enums import *
+from datetime import date
 
 class UserOutput(BaseModel):
     id:         UUID
     username:   str
-    fullname:   str
+    fullname:   Union[str, None]
     email:      str
-    DoB:        datetime
+    DoB:        Union[datetime, None]      
     gender:     GenderEnum          
     createdAt:  datetime         
     updatedAt:  datetime
