980fb4fd73f70584d5dbba523513dc5566a9d583
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-06-07 23:35:20 +0330

    in test mode redis should use localhost

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -6,17 +6,20 @@ from sqlalchemy.pool import StaticPool
 from settings import settings
 from db.engine import Base, get_db
 from main import app
+from utils.redis_utils import redis
 import asyncio
+
+redis.connection_pool.connection_kwargs['host'] = '127.0.0.1'
 
 engine = create_async_engine(
     settings.SQLALCHEMY_SQLITE_DATABASE_URL,
     connect_args={"check_same_thread": False},
     poolclass=StaticPool,
 )
-TestingSessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
+TestingSessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
 
 
-# asyncio(Base.metadata.create_all)
+# asyncio(engine.begin() metaBase.metadata.create_all)
 
 
 async def override_get_db():
@@ -27,6 +30,7 @@ async def override_get_db():
         await db.close()
 
 app.dependency_overrides[get_db] = override_get_db
+# app.dependency_overrides[lifespan] =  
 
 client = TestClient(
     app,
