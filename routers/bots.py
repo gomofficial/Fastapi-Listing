2ebf1b005b94671237b99bab8aa49673564d47a2
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-06-09 22:24:25 +0330

    weather route description

--- routers/bots.py
+++ routers/bots.py
@@ -6,6 +6,16 @@ bots_router = APIRouter()
 
 @bots_router.post('/')
 async def get_weather(days:int, city:str):
+    '''
+        description : weather api using celery \n
+        id : \n
+            city "city name query param" \n
+            days "days number query param" \n
+        body : \n
+            None
+        output :\n 
+            csv file
+    '''
     task = weather_forcast.delay(days=days, city=city)
 
     return {"taskId":task.task_id, "message": "your task is in task queue and will be saved in Forcast.csv"}
